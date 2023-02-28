5959650973af26e49ac0c14bed7b0bf72d41328d
yousufmiah <yousufweb99@gmail.com>
2023-02-28 17:38:11 +0600

    profileDetails

--- src/routes/api.js
+++ src/routes/api.js
@@ -8,32 +8,16 @@ const router = express.Router();
 
 router.post("/registration", UsersController.registration);
 router.post("/login", UsersController.login);
-router.post(
-  "/profileUpdate",
-  AuthVerifyMiddleware,
-  UsersController.profileUpdate
-);
+router.post(  "/profileUpdate",  AuthVerifyMiddleware,  UsersController.profileUpdate);
+router.get("/profileDetails",AuthVerifyMiddleware,UsersController.profileDetails);
+
 
 router.post("/createTask", AuthVerifyMiddleware, TasksController.createTask);
-router.get(
-  "/updateTaskStatus/:id/:status",
-  AuthVerifyMiddleware,
-  TasksController.updateTaskStatus
-);
-
+router.get(  "/updateTaskStatus/:id/:status",  AuthVerifyMiddleware,  TasksController.updateTaskStatus);
 router.get("/deleteTask/:id", AuthVerifyMiddleware, TasksController.deleteTask);
+router.get(  "/listTaskByStatus/:status",  AuthVerifyMiddleware,  TasksController.listTaskByStatus);
 
-router.get(
-  "/listTaskByStatus/:status",
-  AuthVerifyMiddleware,
-  TasksController.listTaskByStatus
-);
-
-router.get(
-  "/taskStatusCount",
-  AuthVerifyMiddleware,
-  TasksController.taskStatusCount
-);
+router.get(  "/taskStatusCount",  AuthVerifyMiddleware,  TasksController.taskStatusCount);
 
 // router.get(
 //   "/SelectProfile",
