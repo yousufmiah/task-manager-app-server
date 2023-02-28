6683761f57342ab46ef2edcfe479ef5699df2fe8
yousufmiah <yousufweb99@gmail.com>
2023-02-28 16:35:43 +0600

    limited

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(helmet());
 app.use(mongoSanitize());
 app.use(xssClean());
 app.use(hpp());
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({limit:"50mb"}))
+
+
 
 //bodyParser implement
 app.use(bodyParser.json()); //bodyParser er vitore json kaj korchi tai
